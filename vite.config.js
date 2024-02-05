e79a105ea73ad4b9d7a941cf900fc0f941a514a7
SAVe1831 <save1831537@gmail.com>
2024-02-05 13:08:01 +0300

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import compressionPlugin from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/resume/`,
+  build: {
+    minify: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
+  base: `/resume`,
   plugins: [
     vue(),
+    compressionPlugin({
+      ext: '.gz', // Расширение для сжатых файлов
+      algorithm: 'gzip', // Используемый алгоритм сжатия (gzip или brotli)
+      threshold: 10240, // Минимальный размер файла для сжатия (в байтах)
+      deleteOriginFile: false // Удалить исходные файлы после сжатия
+    })
   ],
   resolve: {
     alias: {
