bbbbfcd7fd8406671424154da86674b00179cf21
SAVe1831 <save1831537@gmail.com>
2024-02-06 13:37:52 +0300

    added hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "@/pages/Home.vue";
 import Education from "@/pages/Education.vue";
@@ -46,7 +46,7 @@ const routes = [
 
 const router = createRouter({
     routes,
-    history: createWebHistory()
+    history: createWebHashHistory()
 })
 
 export default router;
