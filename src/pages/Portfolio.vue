8e1170c67077d46ea45fb2e913fc9a323be3f72d
SAVe1831 <save1831537@gmail.com>
2024-07-20 20:19:03 +0300

    corrected portfolio

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -8,11 +8,17 @@
             <div class="content-container">
                 <h4>Пока список выглядит скромно, но обязательно будет стремительно увеличиваться!</h4>
                 <p>1. <a href="https://save1831.github.io/online_store">Онлайн-магазин товаров для домашних животных</a> и соответствующая <a href="https://github.com/SAVe1831/online_store">ссылка на GitHub</a></p>
+                <p class="description">Написан на фреймворке Vue3 Composition API.<br> В процессе разработки использованы следующие зависимости:<br>vue<br> vue-router<br> primeflex<br> primeicons<br> primevue<br> axios<br> lodash.debounce<br> pinia<br> vite<br> gh-pages.</p>
                 <p>2. <a href="https://save1831.github.io/Photographer-s-website">Веб-сайт фотографа</a> и соответствующая <a href="https://github.com/SAVe1831/Photographer-s-website">ссылка на GitHub</a></p>
+                <p class="description">Написан на фреймворке Vue3 Composition API.<br> В процессе разработки использованы следующие зависимости:<br> vue<br> vue-router<br> primeflex<br> primevue<br> axios<br> vite<br> vitejs/plugin-vue<br> gh-pages.</p>
                 <p>3. <a href="https://save1831.github.io/The-Rick-and-Morty-API">The-Rick-and-Morty-API (фильтрация)</a> и соответствующая <a href="https://github.com/SAVe1831/The-Rick-and-Morty-API">ссылка на GitHub</a></p>
+                <p class="description">Написан на фреймворке Vue3 Composition API.<br> В процессе разработки использованы следующие зависимости:<br> vue<br> primeflex<br> axios<br> vite<br> vitejs/plugin-vue<br> gh-pages.</p>
                 <p>4. <a href="https://save1831.github.io/hangman/">Hangman (игра Виселица)</a> и соответствующая <a href="https://github.com/SAVe1831/hangman">ссылка на GitHub</a></p>
+                <p class="description">Написан на фреймворке Vue3 Composition API.<br> В процессе разработки использованы следующие зависимости:<br> vue<br> axios<br> rushstack/eslint-patch<br> tsconfig/node20<br> types/node<br> vitejs/plugin-vue<br> vue/eslint-config-prettier<br> vue/eslint-config-typescript<br> vue/tsconfig<br> eslint<br> eslint-plugin-vue<br> gh-pages<br> npm-run-all2<br> prettier<br> Typescript<br> vite<br> vue-tsc.</p>
                 <p>5. <a href="https://save1831.github.io/favorite_movies">Топ-50 фильмов Кинопоиска</a> и соответствующая <a href="https://github.com/SAVe1831/favorite_movies">ссылка на GitHub</a></p>
-                <p>6. <a href="https://save1831.github.io/resume">Данное резюме</a> и соответствующая <a href="https://github.com/SAVe1831/resume">ссылка на GitHub</a></p><br>
+                <p class="description">Написан на фреймворке Vue3 Composition API.<br> В процессе разработки использованы следующие зависимости:<br> vue<br> axios<br> pinia<br> vite<br> vitejs/plugin-vue<br> gh-pages</p>
+                <p>6. <a href="https://save1831.github.io/resume">Данное резюме</a> и соответствующая <a href="https://github.com/SAVe1831/resume">ссылка на GitHub</a></p>
+                <p class="description">Написан на фреймворке Vue3 Options API.<br> В процессе разработки использованы следующие зависимости:<br> vue<br> vue-router<br> primeflex<br> primevue<br> primeicons<br> vite<br> vitejs/plugin-vue<br> vite-plugin-compression<br> gh-pages</p><br>
                 <p>А это просто ссылка на <a href="https://github.com/SAVe1831?tab=repositories">мой GitHub</a> - там есть что-то ещё.</p>
             </div>
         </div>
@@ -56,6 +62,12 @@ h2 {
     border-radius: 6px;
     padding: 10px;
     
+}
+p {
+    margin: 0;
+}
+.description {
+    margin-bottom: 12px;
 }
 @media (max-width: 750px) {
     .head-container {
